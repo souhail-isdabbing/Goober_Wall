d2490c84a880ff8aaae11bfbce6d45b0167bdd6f
unknown <souhail.elmazouni2006@gmail.com>
2024-06-26 22:31:30 +0100

    add: edited stuff

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,8 +6,8 @@ function Modal({image,name,closing}){
     return(
         <div className='bg-black-rgba fixed z-50 inset-0 flex justify-center items-center '>
             <div className=' modal_fig m-10'>
-                <button className='absolute top-2 right-2' onClick={closing}><img className='size-4' src='/src/close.png'></img></button>
-                <img className='modal_img w-full mx-auto'src={`/src/${image}`} alt="my_photo"></img>
+                <button className='absolute top-2 right-2' onClick={closing}><img className='size-4' src='./src/close.png'></img></button>
+                <img className='modal_img w-full mx-auto'src={`./src/${image}`} alt="my_photo"></img>
                 <p className='modal_cap'>{name}</p>
             </div>
         </div>
